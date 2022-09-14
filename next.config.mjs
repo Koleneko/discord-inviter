90d1523a06651b9b3422b8013b0d09f8fd466ca4
vkoleneko <vkoleneko@gmail.com>
2022-09-14 23:21:19 +0500

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -15,9 +15,12 @@ function defineNextConfig(config) {
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["cdn.discordapp.com"],
+  },
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ["ru"],
+    defaultLocale: "ru",
   },
 });
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,8 +3,8 @@ import DiscordProvider from "next-auth/providers/discord";
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
-import { env } from "../../../env/server.mjs";
+import { prisma } from "@/server/db/client";
+import { env } from "@/env/server.mjs";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -16,14 +16,12 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
 };
 
